ff043735d852985d4eb17d070153d2313d018fcf
Cosmic <mertkipge@gmail.com>
2026-09-13 17:26:30 -0400

    sdfs

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+// gcc -std=c17 -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Werror main.c -o
+// main && .\main
+
 char *cleanedString(char *s)
 {
     s[strcspn(s, "\n")] = '\0';
