f98e7c5ce3920532867f0b888d5ab0a612ea7ee2
cosmic <84875247+Online5277@users.noreply.github.com>
2026-09-13 17:19:02 -0400

    aasd

--- main.c
+++ main.c
@@ -1,49 +1,56 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-char* cleanedString(char* s) {
-  s[strcspn(s, "\n")] = '\0';
-  return s;
+char *cleanedString(char *s)
+{
+    s[strcspn(s, "\n")] = '\0';
+    return s;
 }
 
-static void greetUser() {
-  bool hasName = false;
-  do {
-    fflush(stdin);
-  printf("Hello, enter your first name: ");
-  char firstName[256];
-  if (fgets(firstName, sizeof(firstName), stdin) == NULL) {
+static void greetUser(char *fn, char *ln)
+{
+    bool hasName = false;
+    do
+    {
+        printf("Hello, enter your first name: ");
+        char firstName[256];
+        if (fgets(firstName, sizeof(firstName), stdin) == NULL)
+        {
             return;
         }
 
-  printf("Thank you, %s, now enter your last name: ", cleanedString(firstName));
-  char lastName[256];
-  fgets(lastName, sizeof(lastName), stdin);
+        printf("Thank you, %s, now enter your last name: ",
+               cleanedString(firstName));
+        char lastName[256];
+        if (fgets(lastName, sizeof(lastName), stdin) == NULL)
+        {
+            return;
+        }
 
-  printf("Your name is: %s %s, is this true? [y] or [n]: ", firstName, cleanedString(lastName));
-  char choice[3];
-  fgets(choice, sizeof(choice), stdin);
+        printf("Your name is: %s %s, is this true? [y] or [n]: ", firstName,
+               cleanedString(lastName));
+        char choice[256];
+        if (fgets(choice, sizeof(choice), stdin) == NULL)
+        {
+            return;
+        }
 
-  printf("You chose %c\n", choice[0]);
-  
-  if (choice[0] == 'y') {
-      hasName = true;
-      // return firstName, lastName;
-    } else {
-      hasName = false;
-      printf("Rerunning greetUser()...\n");
-    }
-  } while (!hasName); 
+        printf("You chose %c\n", choice[0]);
+
+        choice[0] == 'y' ? hasName = true : false;
+        strcpy_s(fn, sizeof(firstName), firstName);
+        strcpy_s(ln, sizeof(lastName), lastName);
+    } while (!hasName);
 }
 
+int main(void)
+{
+    char firstName[256], lastName[256];
+    // start of program
+    greetUser(firstName, lastName);
 
-int main(void) {
+    printf("%s %s", firstName, lastName);
 
-  greetUser();
-
-
-
-
-  return 0;
+    return 0;
 }
