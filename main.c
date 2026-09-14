e8793ea73ad89c430d63c9ee999b2e2dc443cb36
cosmic <84875247+Online5277@users.noreply.github.com>
2026-09-14 01:06:11 -0400

    fancy code

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -11,47 +12,37 @@ char *cleanedString(char *s)
     return s;
 }
 
-static void greetUser(char *fn, char *ln)
+static bool greetUser(char *fn, int fnSize, char *ln, int lnSize)
 {
-    bool hasName = false;
-    do
+    for (;;)
     {
         printf("Hello, enter your first name: ");
-        char firstName[256];
-        if (fgets(firstName, sizeof(firstName), stdin) == NULL)
-        {
-            return;
-        }
+        if (fgets(fn, fnSize, stdin) == NULL)
+            return false;
+        cleanedString(fn);
 
-        printf("Thank you, %s, now enter your last name: ",
-               cleanedString(firstName));
-        char lastName[256];
-        if (fgets(lastName, sizeof(lastName), stdin) == NULL)
-        {
-            return;
-        }
+        printf("Thank you, %s, now enter your last name: ", fn);
+        if (fgets(ln, lnSize, stdin) == NULL)
+            return false;
+        cleanedString(ln);
 
-        printf("Your name is: %s %s, is this true? [y] or [n]: ", firstName,
-               cleanedString(lastName));
+        printf("Your name is: %s %s, is this true? [y] or [n]: ", fn, ln);
+
         char choice[256];
-        if (fgets(choice, sizeof(choice), stdin) == NULL)
-        {
-            return;
-        }
+        if (fgets(choice, sizeof choice, stdin) == NULL)
+            return false;
 
-        printf("You chose: %c.\n", choice[0]);
-
-        choice[0] == 'y' ? hasName = true : false;
-        strncpy(fn, firstName, 256);
-        strncpy(ln, lastName, 256);
-    } while (!hasName);
+        if (choice[0] == 'y')
+            return true;
+    }
 }
 
 int main(void)
 {
     char firstName[256], lastName[256];
     // start of program
-    greetUser(firstName, lastName);
+    if (!greetUser(firstName, sizeof firstName, lastName, sizeof lastName))
+        return 1;
 
     printf("%s %s", firstName, lastName);
 
