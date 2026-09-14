94480dfaae93bc287ae1ef803fc6f5ea34cc1897
cosmic <84875247+Online5277@users.noreply.github.com>
2026-09-14 02:36:21 -0400

    fishing game before bed

---
+++ stateMachine.c
@@ -0,0 +1,131 @@
+#include "stateMachine.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef enum
+{
+    STATE_IDLE,
+    STATE_ATTACH_BAIT,
+    STATE_CASTING,
+    STATE_REELING,
+    STATE_COOKING,
+    STATE_QUIT
+} GameState;
+
+void fishingGame(char *firstName, char *lastName)
+{
+    GameState state = STATE_IDLE;
+    char input[32];
+
+    bool rodHasBait = false;
+    int baitCount = 10;
+    int fishInventory = 0;
+    int fishingExperience = 0;
+    int cookingExperience = 0;
+
+    printf("Hello, %s %s. It's time to fish.\n", firstName, lastName);
+
+    while (state != STATE_QUIT)
+    {
+        switch (state)
+        {
+        case STATE_IDLE:
+            printf("\nYou are idle... Shouldn't you be fishing in a fishing "
+                   "game?\n\n[b] Attach bait to fishing rod\n[c] Cast fishing "
+                   "rod\n[g] Grill some fish\n[q] Quit the game\n");
+
+            fgets(input, sizeof(input), stdin);
+            if (input[0] == 'q')
+            {
+                state = STATE_QUIT;
+                break;
+            }
+            else if (input[0] == 'b')
+            {
+                state = STATE_ATTACH_BAIT;
+            }
+            else if (input[0] == 'g')
+                state = STATE_COOKING;
+            else if (input[0] == 'c')
+            {
+                if (rodHasBait == true)
+                {
+                    state = STATE_CASTING;
+                }
+                else
+                {
+                    printf("You need bait first!\n");
+                    printf("Press Enter to continue.");
+                    getchar();
+                }
+            }
+            break;
+
+        case STATE_ATTACH_BAIT:
+            if (rodHasBait == false)
+            {
+                printf("You have %d worms\nWould you like to attach one to "
+                       "your rod? [y] or [n]: ",
+                       baitCount);
+                fgets(input, sizeof input, stdin);
+
+                if (input[0] == 'y')
+                {
+                    if (baitCount > 0)
+                    {
+                        rodHasBait = true;
+                        baitCount--;
+                        state = STATE_IDLE;
+                    }
+                }
+                else
+                {
+                    printf("You don't have bait!\n");
+                    state = STATE_QUIT;
+                }
+            }
+            else
+            {
+                printf("You already have bait on your hook!\n");
+                state = STATE_IDLE;
+            }
+            break;
+        case STATE_CASTING:
+            printf("You cast your rod!\n");
+            printf("Press \'enter\' to jiggle your bait to entice a fish.\n");
+            if (getchar())
+            {
+                printf("A fish has biten your bait! Press \'enter\' to reel it "
+                       "in!\n");
+                if (getchar())
+                    state = STATE_REELING;
+            }
+            break;
+        case STATE_REELING:
+            //            printf("Insert logic for fish minigame here\n");
+            printf("You have caught a fish!\n");
+            fishInventory++;
+            fishingExperience += 10;
+            printf("Fishing XP: %d\n", fishingExperience);
+            rodHasBait = false;
+            state = STATE_IDLE;
+            break;
+
+        case STATE_COOKING:
+            if (fishInventory == 0)
+                printf("What did you even plan on cooking? Get to fishing!\n");
+
+            while (fishInventory > 0)
+            {
+                printf("You cook a fish on the grill... [+10 Cooking XP]\n");
+                fishInventory--;
+                cookingExperience += 10;
+                printf("Cooking XP: %d\n", cookingExperience);
+            }
+            state = STATE_IDLE;
+            break;
+        case STATE_QUIT:
+            break;
+        }
+    }
+}
---
+++ stateMachine.h
@@ -0,0 +1,6 @@
+#ifndef STATE_MACHINE_H
+#define STATE_MACHINE_H
+
+void fishingGame(char *, char *);
+
+#endif
